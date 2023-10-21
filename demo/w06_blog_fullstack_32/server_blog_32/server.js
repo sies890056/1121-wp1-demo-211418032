a432c04fc33d60266e3cef66078493d2e415ba9e
sies890056 <sies890056@gmail.com>
2023-10-21 19:25:57 +0800

    w06_blog_fullstack_32

---
+++ demo/w06_blog_fullstack_32/server_blog_32/server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import logger from 'morgan';
+import path from 'path';
+
+import apiCardRouter_32 from "./route/api/apiCardRouter_32.js"
+const app = express();
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import cors from  'cors';
+
+import db from'./utils/database.js';
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(logger('dev'));
+
+
+
+app.use(express.static('public'));
+app.set('view engine','ejs');
+
+app.use(cors());
+
+//app.use('/', (req,res,next)=>{
+//    res.render('index',{title:'Express'});
+//});
+
+const port = process.env.PORT || 5000;
+
+app.use('/api/card_32',apiCardRouter_32);
+
+
+app.listen(port, () => {
+ if(process.env.DATABASE ==='SUPABASE'){
+    console.log(`Connecting SUPABASE PostgreSQL database ${db.options.database}`);
+ }   else{
+console.log(`Connecting local PostgreSQL ${db.options.database}`);
+}
+console.log(`Connecting on port ${port}`);
+} );1
