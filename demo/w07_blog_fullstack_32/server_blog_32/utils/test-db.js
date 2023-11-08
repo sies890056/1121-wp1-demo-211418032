5030a65a4550f6a18cccfa29736fcbfc2f518d09
sies890056 <sies890056@gmail.com>
2023-11-08 21:44:16 +0800

    P1-5: Git push P1 with git log

--- demo/w07_blog_fullstack_32/server_blog_32/utils/test-db.js
+++ demo/w07_blog_fullstack_32/server_blog_32/utils/test-db.js
@@ -3,7 +3,7 @@ const db = require('./database');
 
 const testDB =async()=>{
 try{
-const results =  await db.query(`select *from card_24`);
+const results =  await db.query(`select *from card_32`);
 console.log('json data',results.rows);
 }catch(error){
     console.log(error);
